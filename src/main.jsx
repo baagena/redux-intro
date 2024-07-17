e941df78ba11762ef15a721760cd8cf4c6af2865
baagena <baagena@gmail.com>
2024-07-17 12:26:18 +0200

    add react-redux package in our project

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./store";
+import store from "./store";
+store.dispatch({ type: "account/deposit", payload: 250 });
+console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
