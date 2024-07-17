839d61e50e0925a5e837af9733d597219928ae49
baagena <baagena@gmail.com>
2024-07-17 12:27:00 +0200

    return state instead of return nothing from requestLoan action creator

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -11,7 +11,7 @@ export default function accountReducer(state = initialStateAccount, action) {
         case "account/withdraw":
             return {...state, balance: state.balance - action.payload}
         case "account/requestLoan":
-            if(state.loan > 0) return;
+            if(state.loan > 0) return state;
             return {...state, loan: action.payload.amount, loanPurpose: action.payload.loanPurpose, balance: state.balance + action.payload.amount}
         case "account/payLoan":
             return {...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan}   
